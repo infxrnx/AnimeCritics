c32a09ac6eaba7424554f116efc4e3feac29bade
infxrnx <infxrno@mail.ru>
2024-04-22 02:26:20 +0300

    test test

--- src/main/java/com/project/anime/dto/anime/CreateAnime.java
+++ src/main/java/com/project/anime/dto/anime/CreateAnime.java
@@ -1,9 +1,11 @@
 package com.project.anime.dto.anime;
 
 import java.sql.Date;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 
+@AllArgsConstructor
 @Data
 public class CreateAnime {
   private String title;
