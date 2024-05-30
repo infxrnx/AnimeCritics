686f3789d12f4b472392bdb6aeb5eb2b753eafee
infxrnx <infxrno@mail.ru>
2024-05-30 18:47:23 +0300

    send

--- src/main/java/com/project/anime/controller/InterfaceController.java
+++ src/main/java/com/project/anime/controller/InterfaceController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping()
 public class InterfaceController {
   private final InterfaceService interfaceService;
