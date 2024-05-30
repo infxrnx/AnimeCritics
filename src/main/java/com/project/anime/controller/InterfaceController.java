8b3e5f541c4d336bc6b57c6676c86ade90c011f8
infxrnx <infxrno@mail.ru>
2024-05-30 16:00:30 +0300

    tested

--- src/main/java/com/project/anime/controller/InterfaceController.java
+++ src/main/java/com/project/anime/controller/InterfaceController.java
@@ -18,8 +18,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller("/")
+@RestController()
 public class InterfaceController {
   private final InterfaceService interfaceService;
   private final AnimeService animeService;
